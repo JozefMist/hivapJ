c1c09c04290454b76206eac7579e298c8fd2585c
Adam Sitarcik <adamsitarcik21@gmail.com>
2022-09-12 17:32:12 +0200

    script to extract clear data from hivaperg

--- process_data.py
+++ process_data.py
@@ -1,4 +1,26 @@
 import sys
 import os
+import os.path
+import pathlib
 
+current_path = pathlib.Path().resolve()
+if len(sys.argv) != 2:
+    print('Incorrect input')
+    exit()
+
+file_name = sys.argv[1]
+
+if not sys.argv[1].endswith('.dat'):
+    print('You need to provide .dat file')
+
+file = open(sys.argv[1], 'r', encoding='ascii', errors='ignore')
+lines_in_file = file.readlines()
+
+line_array = []
+
+for line in lines_in_file:
+    line_array.append(line.split())  
+
+for i in line_array:
+    print(i)
 
